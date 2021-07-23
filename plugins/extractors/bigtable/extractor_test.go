bbca91d6c651b0593f47169b3daccfdbee7525dd
Abhishek Sah <abhishek.sah@go-jek.com>
2021-07-24 05:11:07 +0530

    feat: add support for Bigtable metadata extraction (#142)
    * feat: add bigtable extractor
    
    Co-authored-by: Prakhar Mathur <prakharmathur82@gmail.com>
    
    * refactor: fetch tables info concurrently
    
    * test: bigtable metadata extraction
    
    Co-authored-by: Prakhar Mathur <prakharmathur82@gmail.com>
    
    * test: add bigtable emulator for running integration tests and improve docs
    
    Co-authored-by: Prakhar Mathur <prakharmathur82@gmail.com>
    
    Co-authored-by: Prakhar Mathur <prakharmathur82@gmail.com>
    Co-authored-by: Ravi Suhag <suhag.ravi@gmail.com>

---
+++ plugins/extractors/bigtable/extractor_test.go
@@ -0,0 +1,181 @@
+//+build integration
+
+package bigtable
+
+import (
+	"cloud.google.com/go/bigtable"
+	"context"
+	"errors"
+	"github.com/odpf/meteor/plugins/extractors/bigtable/mocks"
+	"github.com/odpf/meteor/plugins/testutils"
+	"github.com/stretchr/testify/mock"
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/odpf/meteor/core/extractor"
+	"github.com/odpf/meteor/logger"
+	"github.com/ory/dockertest/v3"
+	"github.com/ory/dockertest/v3/docker"
+	"github.com/stretchr/testify/assert"
+)
+
+var log = logger.NewWithWriter("info", ioutil.Discard)
+
+func TestMain(m *testing.M) {
+	// setup test
+	opts := dockertest.RunOptions{
+		Repository: "shopify/bigtable-emulator",
+		Env: []string{
+			"BIGTABLE_EMULATOR_HOST=localhost:9035",
+		},
+		ExposedPorts: []string{"9035"},
+		PortBindings: map[docker.Port][]docker.PortBinding{
+			"9035": {
+				{HostIP: "0.0.0.0", HostPort: "9035"},
+			},
+		},
+		Cmd: []string{"-cf", "dev.records.data,dev.records.metadata"},
+	}
+	// exponential backoff-retry, because the application in the container might not be ready to accept connections yet
+	retryFn := func(resource *dockertest.Resource) (err error) {
+		_, err = bigtable.NewAdminClient(context.Background(), "dev", "dev")
+		return
+	}
+	err, purgeFn := testutils.CreateContainer(opts, retryFn)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// run tests
+	code := m.Run()
+
+	if err := purgeFn(); err != nil {
+		log.Fatal(err)
+	}
+	os.Exit(code)
+}
+
+func TestExtract(t *testing.T) {
+	t.Run("should return error if no project_id in config", func(t *testing.T) {
+		extr := New(log)
+		_, err := extr.Extract(map[string]interface{}{
+			"wrong-config": "sample-project",
+		})
+
+		assert.Equal(t, extractor.InvalidConfigError{}, err)
+	})
+
+	t.Run("should return error if project_id is empty", func(t *testing.T) {
+		extr := New(log)
+		_, err := extr.Extract(map[string]interface{}{
+			"project_id": "",
+		})
+
+		assert.EqualError(t, err, "invalid extractor config")
+	})
+
+	t.Run("should return bigtable metadata on success", func(t *testing.T) {
+		extr := New(log)
+		os.Setenv("BIGTABLE_EMULATOR_HOST", "localhost:9035")
+		oldInstanceAdminClientCreator := instanceAdminClientCreator
+		oldInstanceInfoGetter := instanceInfoGetter
+
+		defer func() {
+			instanceAdminClientCreator = oldInstanceAdminClientCreator
+			instanceInfoGetter = oldInstanceInfoGetter
+		}()
+
+		instanceAdminClientCreator = func(_ context.Context, _ Config) (*bigtable.InstanceAdminClient, error) {
+			return nil, nil
+		}
+
+		instanceInfoGetter = func(_ context.Context, _ InstancesFetcher) (instanceNames []string, err error) {
+			instanceNames = append(instanceNames, "dev")
+			return
+		}
+
+		result, err := extr.Extract(map[string]interface{}{
+			"project_id": "dev",
+		})
+		assert.Nil(t, err)
+		assert.Equal(t, 1, len(result))
+		assert.Equal(t, "dev.dev.records", result[0].Urn)
+		assert.Equal(t, "bigtable", result[0].Source)
+		assert.Equal(t, "records", result[0].Name)
+		assert.Equal(t, 1, len(result[0].Custom.CustomProperties))
+		os.Unsetenv("BIGTABLE_EMULATOR_HOST")
+	})
+
+	t.Run("should handle instance admin client initialization error", func(t *testing.T) {
+		extr := New(log)
+		os.Setenv("BIGTABLE_EMULATOR_HOST", "localhost:9035")
+		oldInstanceAdminClientCreator := instanceAdminClientCreator
+		oldInstanceInfoGetter := instanceInfoGetter
+
+		defer func() {
+			instanceAdminClientCreator = oldInstanceAdminClientCreator
+			instanceInfoGetter = oldInstanceInfoGetter
+		}()
+
+		instanceAdminClientCreator = func(_ context.Context, _ Config) (*bigtable.InstanceAdminClient, error) {
+			return nil, errors.New("random error")
+		}
+
+		result, err := extr.Extract(map[string]interface{}{
+			"project_id": "dev",
+		})
+		assert.EqualError(t, err, "random error")
+		assert.Nil(t, result)
+		os.Unsetenv("BIGTABLE_EMULATOR_HOST")
+	})
+
+	t.Run("should handle errors in getting instances list", func(t *testing.T) {
+		extr := New(log)
+		os.Setenv("BIGTABLE_EMULATOR_HOST", "localhost:9035")
+		oldInstanceAdminClientCreator := instanceAdminClientCreator
+		oldInstanceInfoGetter := instanceInfoGetter
+
+		defer func() {
+			instanceAdminClientCreator = oldInstanceAdminClientCreator
+			instanceInfoGetter = oldInstanceInfoGetter
+		}()
+
+		instanceAdminClientCreator = func(_ context.Context, _ Config) (*bigtable.InstanceAdminClient, error) {
+			return nil, nil
+		}
+
+		instanceInfoGetter = func(_ context.Context, _ InstancesFetcher) (instanceNames []string, err error) {
+			return nil, errors.New("random error")
+		}
+
+		result, err := extr.Extract(map[string]interface{}{
+			"project_id": "dev",
+		})
+		assert.EqualError(t, err, "random error")
+		assert.Nil(t, result)
+		os.Unsetenv("BIGTABLE_EMULATOR_HOST")
+	})
+
+	t.Run("should return instances list from bigtable", func(t *testing.T) {
+		mockedInstanceFetcher := mocks.InstancesFetcher{}
+		instance1Info := &bigtable.InstanceInfo{Name: "foo"}
+		instance2Info := &bigtable.InstanceInfo{Name: "bar"}
+		instances := []*bigtable.InstanceInfo{instance1Info, instance2Info}
+		mockedInstanceFetcher.On("Instances", mock.Anything).Return(instances, nil).Once()
+
+		result, err := getInstancesInfo(context.Background(), &mockedInstanceFetcher)
+		assert.Equal(t, []string{"foo", "bar"}, result)
+		assert.Nil(t, err)
+	})
+
+	t.Run("should handle error on fetching instances list from bigtable", func(t *testing.T) {
+		mockedInstanceFetcher := mocks.InstancesFetcher{}
+		mockedInstanceFetcher.On("Instances", mock.Anything).
+			Return(nil, errors.New("random error")).Once()
+
+		result, err := getInstancesInfo(context.Background(), &mockedInstanceFetcher)
+		assert.EqualError(t, err, "random error")
+		assert.Nil(t, result)
+	})
+}
