a7b75f5f48d6ea147a18d6d96eb42224f076997c
Anwar Hidayat <anwarhidayat14@gmail.com>
2021-07-23 17:04:08 +0700

    feat: add extractor for grafana

--- plugins/extractors/populate.go
+++ plugins/extractors/populate.go
@@ -1,9 +1,12 @@
 package extractors
 
 import (
+	"net/http"
+
 	"github.com/odpf/meteor/core/extractor"
 	"github.com/odpf/meteor/plugins"
 	"github.com/odpf/meteor/plugins/extractors/bigquery"
+	"github.com/odpf/meteor/plugins/extractors/grafana"
 	"github.com/odpf/meteor/plugins/extractors/kafka"
 	"github.com/odpf/meteor/plugins/extractors/mongodb"
 	"github.com/odpf/meteor/plugins/extractors/mssql"
@@ -25,4 +28,7 @@ func PopulateFactory(factory *extractor.Factory, logger plugins.Logger) {
 	factory.SetTableExtractor("mssql", mssql.New)
 	factory.SetTableExtractor("mongodb", mongodb.New)
 	factory.SetTableExtractor("postgres", postgres.New)
+	factory.SetDashboardExtractor("grafana", func() extractor.DashboardExtractor {
+		return grafana.New(&http.Client{}, logger)
+	})
 }
