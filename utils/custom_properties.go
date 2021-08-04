e775c9df73d3868217b349cc05db1feff56e0ef8
Stewart Jingga <stewart_jingga@yahoo.com>
2021-08-04 18:45:24 +0700

    fix(enrich): fix processor to handle streaming process

--- utils/custom_properties.go
+++ utils/custom_properties.go
@@ -5,68 +5,67 @@ import (
 	"github.com/odpf/meteor/proto/odpf/meta/facets"
 )
 
-type CustomPropertiesUpdateFn func(map[string]string) error
-
-func ModifyCustomProperties(data interface{}, updateFn CustomPropertiesUpdateFn) error {
+func GetCustomProperties(data interface{}) map[string]string {
+	var customFacet *facets.Custom
 	switch data := data.(type) {
-	case []meta.Table:
-		return modifyTableCustomProperties(data, updateFn)
-	case []meta.Topic:
-		return modifyTopicCustomProperties(data, updateFn)
-	case []meta.Dashboard:
-		return modifyDashboardCustomProperties(data, updateFn)
+	case meta.Table:
+		customFacet = data.Custom
+	case meta.Topic:
+		customFacet = data.Custom
+	case meta.Dashboard:
+		customFacet = data.Custom
+	default:
+		// skip process if data's type is not defined
+		return nil
 	}
 
-	return nil
+	// if data's custom facet is nil, return new empty custom properties
+	if customFacet == nil {
+		return make(map[string]string)
+	}
+
+	// return a new copy to ensure immutability
+	return copyCustomProperties(customFacet.CustomProperties)
 }
 
-func modifyTableCustomProperties(tables []meta.Table, updateFn CustomPropertiesUpdateFn) (err error) {
-	for i := 0; i < len(tables); i++ {
-		table := &tables[i]
-		if table.Custom == nil {
-			table.Custom = createCustomFacet()
-		}
+func SetCustomProperties(data interface{}, customProps map[string]string) interface{} {
+	switch data := data.(type) {
+	case meta.Table:
+		data.Custom = createOrGetCustomFacet(data.Custom)
+		data.Custom.CustomProperties = customProps
+		return data
+	case meta.Topic:
+		data.Custom = createOrGetCustomFacet(data.Custom)
+		data.Custom.CustomProperties = customProps
+		return data
+	case meta.Dashboard:
+		data.Custom = createOrGetCustomFacet(data.Custom)
+		data.Custom.CustomProperties = customProps
+		return data
+	}
 
-		err = updateFn(table.Custom.CustomProperties)
-		if err != nil {
-			return
+	return data
+}
+
+func createOrGetCustomFacet(facet *facets.Custom) *facets.Custom {
+	if facet == nil {
+		return &facets.Custom{
+			CustomProperties: make(map[string]string),
 		}
+	}
+
+	return facet
+}
+
+func copyCustomProperties(src map[string]string) (dest map[string]string) {
+	if src == nil {
+		return
+	}
+
+	dest = make(map[string]string)
+	for k, v := range src {
+		dest[k] = v
 	}
 
 	return
-}
-func modifyTopicCustomProperties(topics []meta.Topic, updateFn CustomPropertiesUpdateFn) (err error) {
-	for i := 0; i < len(topics); i++ {
-		topic := &topics[i]
-		if topic.Custom == nil {
-			topic.Custom = createCustomFacet()
-		}
-
-		err = updateFn(topic.Custom.CustomProperties)
-		if err != nil {
-			return
-		}
-	}
-
-	return
-}
-func modifyDashboardCustomProperties(dashboards []meta.Dashboard, updateFn CustomPropertiesUpdateFn) (err error) {
-	for i := 0; i < len(dashboards); i++ {
-		dashboard := &dashboards[i]
-		if dashboard.Custom == nil {
-			dashboard.Custom = createCustomFacet()
-		}
-
-		err = updateFn(dashboard.Custom.CustomProperties)
-		if err != nil {
-			return
-		}
-	}
-
-	return
-}
-func createCustomFacet() *facets.Custom {
-	return &facets.Custom{
-		CustomProperties: make(map[string]string),
-	}
 }
