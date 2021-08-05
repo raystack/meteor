11d3b25013cc7338c72b022ffb90b96c3cf6aff5
GrayFlash <gk74533@gmail.com>
2021-08-06 00:00:43 +0530

    docs: add metabase to extractors

--- plugins/extractors/populate.go
+++ plugins/extractors/populate.go
@@ -2,5 +2,6 @@ package extractors
 
 import (
 	_ "github.com/odpf/meteor/plugins/extractors/date"
+	_ "github.com/odpf/meteor/plugins/extractors/metabase"
 	_ "github.com/odpf/meteor/plugins/extractors/postgres"
 )
