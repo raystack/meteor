df45b5b64b01a7e25bcb0a62b1aa05c17ab704fe
Ravi Suhag <suhag.ravi@gmail.com>
2021-08-28 05:19:52 -0700

    feat: add list plugin command (#181)

---
+++ cmd/lint.go
@@ -0,0 +1,25 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/odpf/salt/log"
+
+	"github.com/spf13/cobra"
+)
+
+// LintCmd creates a command object for linting recipes
+func LintCmd(lg log.Logger) *cobra.Command {
+
+	cmd := &cobra.Command{
+		Use:     "lint [path]",
+		Aliases: []string{"l"},
+		Args:    cobra.ExactValidArgs(1),
+		Example: "meteor lint recipe.yaml",
+		Short:   "Validate a receipe",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("Validating recipe...")
+		},
+	}
+	return cmd
+}
