bbfeedb9c77b60a4dcdc3483ccc2a451bf578c01
Stewart Jingga <stewart_jingga@yahoo.com>
2021-10-02 10:35:54 +0700

    refactor: move Config from agent to config package

--- config/config.go
+++ config/config.go
@@ -1,9 +1,24 @@
-package agent
+package config
 
-// Config contains the configuration for the agent.
+import (
+	"github.com/odpf/salt/config"
+)
+
+// Config contains the configuration for meteor.
 type Config struct {
 	LogLevel      string `mapstructure:"LOG_LEVEL" default:"info"`
 	StatsdEnabled bool   `mapstructure:"STATSD_ENABLED" default:"false"`
 	StatsdHost    string `mapstructure:"STATSD_HOST" default:"localhost:8125"`
 	StatsdPrefix  string `mapstructure:"STATSD_PREFIX" default:"meteor"`
+}
+
+func Load() (cfg Config, err error) {
+	err = config.
+		NewLoader(config.WithPath("./")).
+		Load(&cfg)
+	if err != nil {
+		return
+	}
+
+	return
 }
--- main.go
+++ main.go
@@ -5,11 +5,10 @@ import (
 	"os"
 	"strings"
 
-	"github.com/odpf/meteor/agent"
 	"github.com/odpf/meteor/cmd"
+	"github.com/odpf/meteor/config"
 	"github.com/odpf/meteor/metrics"
 	"github.com/odpf/meteor/plugins"
-	"github.com/odpf/salt/config"
 
 	_ "github.com/odpf/meteor/plugins/extractors"
 	_ "github.com/odpf/meteor/plugins/processors"
@@ -23,10 +22,8 @@ const (
 )
 
 func main() {
-	var cfg agent.Config
-	l := config.NewLoader(config.WithPath("./"))
-
-	if err := l.Load(&cfg); err != nil {
+	cfg, err := config.Load()
+	if err != nil {
 		fmt.Printf("ERROR: %s\n", err.Error())
 		os.Exit(1)
 	}
