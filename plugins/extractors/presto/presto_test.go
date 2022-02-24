76eeea41950181959d39b8ead48597f234ee4c95
scortier <onlytoaditya@gmail.com>
2022-02-24 22:21:14 +0530

    test: update presto test

--- plugins/extractors/presto/presto_test.go
+++ plugins/extractors/presto/presto_test.go
@@ -6,7 +6,7 @@ import (
 	"fmt"
 	assetsv1beta1 "github.com/odpf/meteor/models/odpf/assets/v1beta1"
 	"github.com/odpf/meteor/plugins"
-	"github.com/odpf/meteor/plugins/extractors/mariadb"
+	"github.com/odpf/meteor/plugins/extractors/presto"
 	"github.com/odpf/meteor/test/mocks"
 	"github.com/odpf/meteor/test/utils"
 	"github.com/ory/dockertest/v3"
@@ -31,17 +31,17 @@ var (
 )
 
 func TestMain(m *testing.M) {
-	//pwd, err := os.Getwd()
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
+	pwd, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
 	// setup test
 	opts := dockertest.RunOptions{
-		Repository: "starburstdata/presto",
-		Tag:        "350-e.18",
-		//Mounts: []string{
-		//	fmt.Sprintf("%s/etc:/etc:rw", pwd),
-		//},
+		Repository: "ahanaio/prestodb-sandbox",
+		Tag:        "latest",
+		Mounts: []string{
+			fmt.Sprintf("%s/etc:/etc:rw", pwd),
+		},
 		ExposedPorts: []string{"8080"},
 		PortBindings: map[docker.Port][]docker.PortBinding{
 			"8080": {
@@ -52,7 +52,7 @@ func TestMain(m *testing.M) {
 
 	// Exponential backoff-retry for container to accept connections
 	retryFn := func(r *dockertest.Resource) (err error) {
-		dsn := "http://presto@localhost:8080?catalog=default&schema=test_db"
+		dsn := "http://presto@localhost:8080"
 		//dsn - http[s]://user[:pass]@host[:port][?parameters]
 		//dsn := fmt.Sprintf("http://%s:%s@%s?catalog=default&schema=%s", user, pass, host, testDB)
 		db, err = sql.Open("presto", dsn)
@@ -79,7 +79,9 @@ func TestMain(m *testing.M) {
 	code := m.Run()
 
 	// Clean tests
-	db.Close()
+	if err = db.Close(); err != nil {
+		return
+	}
 	if err := purgeFn(); err != nil {
 		fmt.Println("12")
 
@@ -91,7 +93,7 @@ func TestMain(m *testing.M) {
 // TestInit tests the configs
 func TestInit(t *testing.T) {
 	t.Run("should return error for invalid config", func(t *testing.T) {
-		err := mariadb.New(utils.Logger).Init(context.TODO(), map[string]interface{}{
+		err := presto.New(utils.Logger).Init(context.TODO(), map[string]interface{}{
 			"invalid_config": "invalid_config_value",
 		})
 		assert.Equal(t, plugins.InvalidConfigError{}, err)
@@ -100,9 +102,9 @@ func TestInit(t *testing.T) {
 
 // TestExtract tests that the extractor returns the expected result
 func TestExtract(t *testing.T) {
-	t.Run("should return mockdata we generated with mariadb", func(t *testing.T) {
+	t.Run("should return mock data we generated with presto", func(t *testing.T) {
 		ctx := context.TODO()
-		newExtractor := mariadb.New(utils.Logger)
+		newExtractor := presto.New(utils.Logger)
 
 		err := newExtractor.Init(ctx, map[string]interface{}{
 			"connection_url": fmt.Sprintf("%s:%s@tcp(%s)/", user, pass, host),
@@ -128,14 +130,25 @@ func TestExtract(t *testing.T) {
 
 // setup is a helper function to set up the test database
 func setup() (err error) {
-	var queries = []string{
-		fmt.Sprintf("DROP DATABASE IF EXISTS %s", testDB),
-		fmt.Sprintf("CREATE DATABASE %s", testDB),
-		fmt.Sprintf("USE %s;", testDB),
-		fmt.Sprintf(`CREATE USER IF NOT EXISTS '%s'@'%%' IDENTIFIED BY '%s';`, user, pass),
-		fmt.Sprintf(`GRANT ALL PRIVILEGES ON *.* TO '%s'@'%%';`, user),
+	fmt.Println("setup enter")
+
+	// querie for sample testing
+	var querie = []string{
+		"CREATE TABLE memory.default.nation AS SELECT * from tpch.tiny.nation;",
+		"INSERT INTO memory.default.nation SELECT * FROM tpch.tiny.nation;",
+		"SELECT * FROM memory.default.nation;",
 	}
-	if err = execute(db, queries); err != nil {
+	//var queries = []string{
+	//fmt.Sprintf("SHOW %s", testDB),
+	//fmt.Sprintf("SELECT * FROM %s", testDB),
+	//fmt.Sprintf("DROP SCHEMA IF EXISTS %s", testDB),
+	//fmt.Sprintf("CREATE SCHEMA %s", testDB),
+	//fmt.Sprintf("USE %s;", testDB),
+	//fmt.Sprintf(`CREATE USER IF NOT EXISTS '%s'@'%%' IDENTIFIED BY '%s';`, user, pass),
+	//fmt.Sprintf(`GRANT ALL PRIVILEGES ON *.* TO '%s'@'%%';`, user),
+	//}
+	if err = execute(db, querie); err != nil {
+		fmt.Println("exec 1")
 		return
 	}
 
@@ -147,6 +160,7 @@ func setup() (err error) {
 		"INSERT INTO jobs VALUES (2, 'test2', 'test22');",
 	}
 	if err = execute(db, populate); err != nil {
+		fmt.Println("exec 2")
 		return
 	}
 	return
@@ -155,7 +169,9 @@ func setup() (err error) {
 // execute is a helper function to execute a list of queries
 func execute(db *sql.DB, queries []string) (err error) {
 	for _, query := range queries {
-		if _, err = db.Exec(query); err != nil {
+		_, err = db.Exec(query) // Exec not supported by presto client
+		if err != nil {
+			fmt.Println(err)
 			return
 		}
 	}
