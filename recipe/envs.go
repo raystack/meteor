290d3c0c641a656311341e60b093ee626481a782
GrayFlash <gk74533@gmail.com>
2022-02-27 12:50:02 +0530

    feat: try to load variables with METEOR_ prefix, if .env not found

--- recipe/envs.go
+++ recipe/envs.go
@@ -1,16 +1,20 @@
 package recipe
 
 import (
-	"log"
+	"os"
 	"strings"
 
 	"github.com/joho/godotenv"
 )
 
+var (
+	recipeEnvVarPrefix = "METEOR_"
+)
+
 func populateData() map[string]string {
 	data, err := godotenv.Read()
 	if err != nil {
-		log.Fatal(err)
+		return populateDataFromLocal()
 	}
 
 	for key, val := range data {
@@ -19,4 +23,38 @@ func populateData() map[string]string {
 	}
 
 	return data
+}
+
+func populateDataFromLocal() map[string]string {
+	data := make(map[string]string)
+	for _, envvar := range os.Environ() {
+		keyval := strings.SplitN(envvar, "=", 2) // "sampleKey=sample=Value" returns ["sampleKey", "sample=value"]
+		key := keyval[0]
+		val := os.ExpandEnv(keyval[1])
+
+		key, ok := mapToMeteorKey(key)
+		if !ok {
+			continue
+		}
+
+		data[key] = val
+	}
+
+	return data
+}
+
+func mapToMeteorKey(rawKey string) (key string, ok bool) {
+	// we are doing everything in lowercase for case insensitivity
+	key = strings.ToLower(rawKey)
+	meteorPrefix := strings.ToLower(recipeEnvVarPrefix)
+	keyPrefixLen := len(meteorPrefix)
+
+	isMeteorKeyFormat := len(key) > keyPrefixLen && key[:keyPrefixLen] == meteorPrefix
+	if !isMeteorKeyFormat {
+		return
+	}
+	key = key[keyPrefixLen:] // strips prefix - meteor_user_id becomes user_id
+	ok = true
+
+	return
 }
