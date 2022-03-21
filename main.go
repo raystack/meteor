60961d76eaedddb49b0dd2b9ff34f3c3aae24d35
Gaurav Kumar <gk74533@gmail.com>
2022-03-21 10:07:30 +0530

    refactor: move config. log and monitor setup to root.go

--- main.go
+++ main.go
@@ -6,15 +6,11 @@ import (
 	"strings"
 
 	"github.com/odpf/meteor/cmd"
-	"github.com/odpf/meteor/config"
-	"github.com/odpf/meteor/metrics"
-	"github.com/odpf/meteor/plugins"
 
 	_ "github.com/odpf/meteor/plugins/extractors"
 	_ "github.com/odpf/meteor/plugins/processors"
 	_ "github.com/odpf/meteor/plugins/sinks"
 	"github.com/odpf/salt/cmdx"
-	"github.com/odpf/salt/log"
 )
 
 const (
@@ -23,28 +19,8 @@ const (
 )
 
 func main() {
-	cfg, err := config.Load("")
-	if err != nil {
-		fmt.Printf("ERROR: %s\n", err.Error())
-		os.Exit(1)
-	}
-
-	lg := log.NewLogrus(log.LogrusWithLevel(cfg.LogLevel))
-	plugins.SetLog(lg)
-
-	// Setup statsd monitor to collect monitoring metrics
-	var monitor *metrics.StatsdMonitor
-	if cfg.StatsdEnabled {
-		client, err := metrics.NewStatsdClient(cfg.StatsdHost)
-		if err != nil {
-			fmt.Printf("ERROR: %s\n", err.Error())
-			os.Exit(exitError)
-		}
-		monitor = metrics.NewStatsdMonitor(client, cfg.StatsdPrefix)
-	}
-
 	// Execute the root command
-	root := cmd.New(lg, monitor, cfg)
+	root := cmd.New()
 	cmd, err := root.ExecuteC()
 
 	if err == nil {
