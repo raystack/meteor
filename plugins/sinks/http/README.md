6dd22c1e390b8232a839dbcdde8b3317c7f0e580
Gaurav Kumar <gk74533@gmail.com>
2022-07-05 10:05:01 +0530

    feat: initialize http sink

---
+++ plugins/sinks/http/README.md
@@ -0,0 +1,18 @@
+# Http
+
+Compass is a search and discovery engine built for querying application deployments, datasets and meta resources. It can also optionally track data flow relationships between these resources and allow the user to view a representation of the data flow graph.
+
+## Usage
+
+```yaml
+sinks:
+  name: http
+  config:
+    url: https://compass.com/v1beta1/asset
+    headers:
+      Header-1: value11,value12
+```
+
+## Contributing
+
+Refer to the contribution guidelines for information on contributing to this module.
---
+++ plugins/sinks/http/http.go
@@ -0,0 +1,141 @@
+package http
+
+import (
+	"bytes"
+	"context"
+	_ "embed"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+
+	"github.com/odpf/meteor/models"
+	"github.com/odpf/meteor/plugins"
+	"github.com/odpf/meteor/registry"
+	"github.com/odpf/meteor/utils"
+	"github.com/odpf/salt/log"
+	"github.com/pkg/errors"
+)
+
+//go:embed README.md
+var summary string
+
+type Config struct {
+	URL     string            `mapstructure:"url" validate:"required"`
+	Headers map[string]string `mapstructure:"headers"`
+}
+
+var sampleConfig = `
+# The url (hostname and route) of the http service
+url: https://compass.com/route
+# Additional HTTP headers, multiple headers value are separated by a comma
+headers:
+	X-Other-Header: value1, value2
+`
+
+type httpClient interface {
+	Do(*http.Request) (*http.Response, error)
+}
+
+type Sink struct {
+	client httpClient
+	config Config
+	logger log.Logger
+}
+
+func New(c httpClient, logger log.Logger) plugins.Syncer {
+	sink := &Sink{client: c, logger: logger}
+	return sink
+}
+
+func (s *Sink) Info() plugins.Info {
+	return plugins.Info{
+		Description:  "Send metadata to http service",
+		SampleConfig: sampleConfig,
+		Summary:      summary,
+		Tags:         []string{"http", "sink"},
+	}
+}
+
+func (s *Sink) Validate(configMap map[string]interface{}) (err error) {
+	return utils.BuildConfig(configMap, &Config{})
+}
+
+func (s *Sink) Init(ctx context.Context, configMap map[string]interface{}) (err error) {
+	if err = utils.BuildConfig(configMap, &s.config); err != nil {
+		return plugins.InvalidConfigError{Type: plugins.PluginTypeSink}
+	}
+
+	return
+}
+
+func (s *Sink) Sink(ctx context.Context, batch []models.Record) (err error) {
+	for _, record := range batch {
+		metadata := record.Data()
+		s.logger.Info("sinking record to http", "record", metadata.GetResource().Urn)
+
+		payload, err := s.buildPayload(metadata)
+		if err != nil {
+			return errors.Wrap(err, "failed to build http payload")
+		}
+		if err = s.send(payload); err != nil {
+			return errors.Wrap(err, "error sending data")
+		}
+
+		s.logger.Info("successfully sinked record to http", "record", metadata.GetResource().Urn)
+	}
+
+	return
+}
+
+func (s *Sink) Close() (err error) { return }
+
+func (s *Sink) send(payloadBytes []byte) (err error) {
+	// send request
+	req, err := http.NewRequest(http.MethodPatch, s.config.URL, bytes.NewBuffer(payloadBytes))
+	if err != nil {
+		return
+	}
+
+	for hdrKey, hdrVal := range s.config.Headers {
+		hdrVals := strings.Split(hdrVal, ",")
+		for _, val := range hdrVals {
+			req.Header.Add(hdrKey, val)
+		}
+	}
+
+	res, err := s.client.Do(req)
+	if err != nil {
+		return
+	}
+	if res.StatusCode == 200 {
+		return
+	}
+
+	var bodyBytes []byte
+	bodyBytes, err = ioutil.ReadAll(res.Body)
+	if err != nil {
+		return
+	}
+	err = fmt.Errorf("http returns %d: %v", res.StatusCode, string(bodyBytes))
+
+	switch code := res.StatusCode; {
+	case code >= 500:
+		return plugins.NewRetryError(err)
+	default:
+		return err
+	}
+}
+
+func (s *Sink) buildPayload(metadata models.Metadata) ([]byte, error) {
+	var payloadByte []byte
+	return payloadByte, nil
+}
+
+func init() {
+	if err := registry.Sinks.Register("http", func() plugins.Syncer {
+		return New(&http.Client{}, plugins.GetLog())
+	}); err != nil {
+		panic(err)
+	}
+}
