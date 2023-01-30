d21f875b314a7b222e9f0ca7ba033711cf7b3a18
Chief-Rishab <f20190124@hyderabad.bits-pilani.ac.in>
2023-01-31 02:09:24 +0530

    GCS: add tests

--- plugins/sinks/gcs/gsc_test.go
+++ plugins/sinks/gcs/gsc_test.go
@@ -8,8 +8,13 @@ import(
 
 	"github.com/stretchr/testify/assert"
 	"github.com/odpf/meteor/plugins"
+	"google.golang.org/protobuf/types/known/anypb"
 	g "github.com/odpf/meteor/plugins/sinks/gcs"
 	testUtils "github.com/odpf/meteor/test/utils"
+	"github.com/odpf/meteor/utils"
+	"github.com/stretchr/testify/mock"
+	v1beta2 "github.com/odpf/meteor/models/odpf/assets/v1beta2"
+	"github.com/odpf/meteor/models"
 )
 
 var validConfig = map[string]interface{}{
@@ -36,7 +41,7 @@ func TestInit(t *testing.T){
 			"project_id":   "google-project-id",
 			"path":         "bucket_name/target_folder",
 		}})
-		assert.ErrorContains(t, actualError, "failed to create client")
+		assert.ErrorContains(t, actualError, "credentials are not specified, failed to create client")
 	})
 
 	t.Run("should retun error if unable to decode base64 service account key", func(t *testing.T){
@@ -49,4 +54,59 @@ func TestInit(t *testing.T){
 		}})
 		assert.ErrorContains(t, actualError, "failed to decode base64 service account")
 	})
+}
+
+func TestSink(t *testing.T){
+
+	t.Run("should write data in bucket and return nil error on success", func(t *testing.T){
+		u := &v1beta2.User{
+			FullName: "John Doe",
+			Email:    "john.doe@odpf.com",
+			Attributes: utils.TryParseMapToProto(map[string]interface{}{
+				"org_unit_path": "/",
+				"aliases":       "doe.john@odpf.com,johndoe@odpf.com",
+			}),
+		}
+		user, _ := anypb.New(u)
+		data := &v1beta2.Asset{
+			Data: user,
+		}
+		jsonBytes, _ := models.ToJSON(data)
+
+		ctx := context.TODO()
+		client := new(mockClient)
+		client.On("WriteData", jsonBytes).Return(nil)
+		client.On("WriteData", []byte("\n")).Return(nil)
+
+		gcsSink := g.New(testUtils.Logger)
+
+		err := gcsSink.Init(context.TODO(), plugins.Config{RawConfig: map[string]interface{}{
+			"project_id":              "google-project-id",
+			"path":                    "bucket_name/target_folder",
+			"service_account_json":  `{"type": "service_account"}`, 
+		}})
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		err = gcsSink.Sink(ctx,[]models.Record{models.NewRecord(data)})
+		
+		assert.NoError(t,err)
+	})
+}
+
+type mockClient struct{
+	mock.Mock
+}
+
+func (m *mockClient) WriteData(jsonBytes []byte) error{
+	args := m.Called(jsonBytes)
+
+	return args.Error(0)
+} 
+
+func (m *mockClient) Close() error{
+	args := m.Called()
+
+	return args.Error(0)
 }
