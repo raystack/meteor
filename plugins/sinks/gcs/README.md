77cf2dc2bcffc1ead81587c7b66226333a927d00
Chief-Rishab <f20190124@hyderabad.bits-pilani.ac.in>
2023-01-31 12:37:37 +0530

    GCS: add sink, client and documentation

---
+++ plugins/sinks/gcs/README.md
@@ -0,0 +1,41 @@
+# GCS
+
+Sinks json data to a file in `ndjson` format in a Google Cloud Storage bucket
+
+## Usage
+```yaml
+sinks:
+  - name: gcs
+    config:
+     project_id: google-project-id
+     path: bucket_name/target_folder
+     object_prefix : github-users
+     service_account_base64: <base64 encoded service account key>
+     service_account_json:
+      {
+        "type": "service_account",
+        "private_key_id": "xxxxxxx",
+        "private_key": "xxxxxxx",
+        "client_email": "xxxxxxx",
+        "client_id": "xxxxxxx",
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "xxxxxxx",
+        "client_x509_cert_url": "xxxxxxx",
+      }
+```
+
+## Config Definition
+
+| Key | Value | Example | Description |  |
+| :-- | :---- | :------ | :---------- | :-- |
+|`project_id` | `string` | `google-project-id` | Google Cloud Storage Project ID  | *required*|
+| `path` | `string` | `bucket_name/target_folder` | the path of the folder where new object is to be put | *required* |
+| `object_prefix` | `string` | `github-users` | the .ndjson file name prefix where json data will be inserted with timestamp </b></b> Note: If prefix is not provided, the output data will be put in a `timestamp.ndjson` file in the provided path. Otherwise in the given example the output file will be `github-users-timestamp.ndjson`| *optional* |
+| `service_account_base64` | `string` | `ewog....fQo=` |  Service Account Key in base64 encoded string. Takes precedence over `service_account_json` value | *optional* |
+| `service_account_json` | `string` | `{"private_key": .., "private_id": ...}` |   Service Account Key in JSON string | *optional* |
+
+
+## Contributing
+
+Refer to the [contribution guidelines](../../../docs/docs/contribute/guide.md#adding-a-new-extractor) for information on contributing to this module.
---
+++ plugins/sinks/gcs/gcs.go
@@ -0,0 +1,161 @@
+package gcs
+
+import (
+	"context"
+	_ "embed"
+	"encoding/base64"
+	"strings"
+	"time"
+
+	"github.com/MakeNowJust/heredoc"
+	"github.com/odpf/meteor/models"
+	assetsv1beta2 "github.com/odpf/meteor/models/odpf/assets/v1beta2"
+	"github.com/odpf/meteor/plugins"
+	"github.com/odpf/meteor/registry"
+	"github.com/odpf/salt/log"
+	"github.com/pkg/errors"
+)
+
+//go:embed README.md
+var summary string
+
+type Config struct {
+	ProjectID            string `mapstructure:"project_id" validate:"required"`
+	Path                 string `mapstructure:"path" validate:"required"`
+	ObjectPrefix         string `mapstructure:"object_prefix"`
+	ServiceAccountJSON   string `mapstructure:"service_account_json"`
+	ServiceAccountBase64 string `mapstructure:"service_account_base64"`
+}
+
+var info = plugins.Info{
+	Description: "saves data in google cloud storage bucket",
+	Summary:     summary,
+	SampleConfig: heredoc.Doc(`
+	project_id: google-project-id
+	path: bucket_name/target_folder
+	object_prefix : github-users
+	service_account_base64: ____base64_encoded_service_account____
+    service_account_json: |-
+      {
+       "type": "service_account",
+       "private_key_id": "xxxxxxx",
+       "private_key": "xxxxxxx",
+       "client_email": "xxxxxxx",
+       "client_id": "xxxxxxx",
+       "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+       "token_uri": "https://oauth2.googleapis.com/token",
+       "auth_provider_x509_cert_url": "xxxxxxx",
+       "client_x509_cert_url": "xxxxxxx"
+     }
+	`),
+	Tags: []string{"gcs", "sink"},
+}
+
+type Sink struct {
+	plugins.BasePlugin
+	logger log.Logger
+	client GCSClient
+	config Config
+}
+
+func New(logger log.Logger) plugins.Syncer {
+	s := &Sink{
+		logger: logger,
+	}
+	s.BasePlugin = plugins.NewBasePlugin(info, &s.config)
+
+	return s
+}
+
+func (s *Sink) Init(ctx context.Context, config plugins.Config) (err error) {
+	if err = s.BasePlugin.Init(ctx, config); err != nil {
+		return err
+	}
+
+	if err := s.validateServiceAccountKey(); err != nil {
+		return err
+	}
+
+	bucketname, objectname := s.resolveBucketandObjectNames()
+
+	if s.client, err = newGCSClient(ctx, []byte(s.config.ServiceAccountJSON), bucketname, objectname); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *Sink) validateServiceAccountKey() error {
+
+	if s.config.ServiceAccountBase64 == "" && s.config.ServiceAccountJSON == "" {
+		return errors.New("credentials are not specified, failed to create client")
+	}
+
+	if s.config.ServiceAccountBase64 != "" {
+		serviceAccountJSON, err := base64.StdEncoding.DecodeString(s.config.ServiceAccountBase64)
+		if err != nil || len(serviceAccountJSON) == 0 {
+			return errors.Wrap(err, "failed to decode base64 service account")
+		}
+		s.config.ServiceAccountJSON = string(serviceAccountJSON)
+	}
+	return nil
+}
+
+func (s *Sink) resolveBucketandObjectNames() (string, string) {
+	dirs := strings.Split(s.config.Path, "/")
+	bucketname := dirs[0]
+	timestamp := time.Now().Format("2006.01.02 15:04:05")
+
+	if s.config.ObjectPrefix != "" {
+		s.config.ObjectPrefix = s.config.ObjectPrefix + "-"
+	}
+
+	objectname := s.config.ObjectPrefix + timestamp + ".ndjson"
+	if len(dirs) > 1 {
+		objectname = dirs[len(dirs)-1] + "/" + s.config.ObjectPrefix + timestamp + ".ndjson"
+	}
+
+	return bucketname, objectname
+}
+
+func (s *Sink) Sink(ctx context.Context, batch []models.Record) (err error) {
+	data := make([]*assetsv1beta2.Asset, 0, len(batch))
+
+	for _, record := range batch {
+		data = append(data, record.Data())
+	}
+	if err = s.writeData(data); err != nil {
+		return errors.Wrap(err, "error in writing data to the object")
+	}
+	return nil
+}
+
+func (s *Sink) writeData(data []*assetsv1beta2.Asset) (err error) {
+	for _, asset := range data {
+		jsonBytes, _ := models.ToJSON(asset)
+
+		if err := s.client.WriteData(jsonBytes); err != nil {
+			return err
+		}
+
+		if err := s.client.WriteData([]byte("\n")); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (s *Sink) Close() (err error) {
+	if err := s.client.Close(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func init() {
+	if err := registry.Sinks.Register("gcs", func() plugins.Syncer {
+		return New(plugins.GetLog())
+	}); err != nil {
+		panic(err)
+	}
+}
